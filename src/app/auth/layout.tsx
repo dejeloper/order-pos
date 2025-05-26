b031867f9bd6f10da9ef4a90274b2617e9eb71fb
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-05-26 00:00:18 -0500

    refactor: reorganize imports and adjust logo height in AuthLayout component

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+import Image from "next/image";
+import {ThemeProvider} from "@/context/ThemeContext";
+
+import {Toaster} from "react-hot-toast";
+
 import GridShape from "@/components/common/GridShape";
 import ThemeTogglerTwo from "@/components/common/ThemeTogglerTwo";
-
-import { ThemeProvider } from "@/context/ThemeContext";
-import Image from "next/image";
-import Link from "next/link"; 
 
 export default function AuthLayout({
   children,
@@ -16,14 +18,14 @@ export default function AuthLayout({
         <div className="relative flex lg:flex-row w-full h-screen justify-center flex-col  dark:bg-gray-900 sm:p-0">
           {children}
           <div className="lg:w-1/2 w-full h-full bg-brand-950 dark:bg-white/5 lg:grid items-center hidden">
-            <div className="relative items-center justify-center  flex z-1"> 
-             
+            <div className="relative items-center justify-center  flex z-1">
+
               <GridShape />
               <div className="flex flex-col items-center max-w-xs">
                 <Link href="/" className="block mb-4">
                   <Image
                     width={231}
-                    height={48}
+                    height={63}
                     src="/images/logo/logo-dark.png"
                     alt="Logo"
                   />
@@ -38,6 +40,16 @@ export default function AuthLayout({
             <ThemeTogglerTwo />
           </div>
         </div>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            style: {
+              background: "#1f2937",
+              color: "#fff",
+              borderRadius: "8px",
+            },
+          }}
+        />
       </ThemeProvider>
     </div>
   );
