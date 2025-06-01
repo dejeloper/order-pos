3ed86b59770ec58d0e5add35169006669242bd4c
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-05-31 22:20:07 -0500

    refactor: add title prop to PagesWrapper for improved flexibility in rendering

--- src/components/common/Wrapper.tsx
+++ src/components/common/Wrapper.tsx
@@ -2,16 +2,21 @@ import PageBreadcrumb from "./PageBreadCrumb";
 
 export default function PagesWrapper({
 	breadcrumbItems,
+	title,
 	children
 }: {
-	breadcrumbItems: { name: string; href?: string }[];
-	children: React.ReactNode;
-}) { 
-	return	(
-	<>
-	<PageBreadcrumb items={breadcrumbItems} /> 
-			 <div className="min-h-[calc(100dvh-180px)] rounded-2xl border border-gray-200 bg-white px-5 py-7 dark:border-gray-800 dark:bg-white/[0.03] xl:px-5 xl:py-7 text-gray-800 dark:text-white/90"> 
-         {children} 
-      </div>
-	</>)
+	breadcrumbItems: {name: string; href?: string}[];
+	title?: string;
+	children?: React.ReactNode;
+}) {
+	return (
+		<>
+			<PageBreadcrumb items={breadcrumbItems} />
+			<div className="min-h-[calc(100dvh-180px)] rounded-2xl border border-gray-200 bg-white px-5 py-7 dark:border-gray-800 dark:bg-white/[0.03] xl:px-5 xl:py-7 text-gray-800 dark:text-white/90">
+				{title &&
+					(<h3 className="mb-2 font-semibold text-theme-xl sm:text-2xl text-center">{title}</h3>)
+				}
+				{children}
+			</div>
+		</>)
 }
